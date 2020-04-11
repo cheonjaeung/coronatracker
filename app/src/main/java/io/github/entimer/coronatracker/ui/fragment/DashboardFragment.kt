7972d9cce7fcbf8c73e106060f1128268cae777d
cheonjaewoong <cheonjaewoong@gmail.com>
2020-04-11 15:11:13 +0900

    Create worldwide card layout.

--- app/src/main/java/io/github/entimer/coronatracker/ui/fragment/DashboardFragment.kt
+++ app/src/main/java/io/github/entimer/coronatracker/ui/fragment/DashboardFragment.kt
@@ -6,10 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import io.github.entimer.coronatracker.R
-import kotlin.math.log
+import kotlinx.android.synthetic.main.layout_card_worldwide.view.*
 
 class DashboardFragment: Fragment() {
+    private var confirmed = 1696139
+    private var recovered = 376200
+    private var death = 102669
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater!!.inflate(R.layout.fragment_dashboard, container, false)
+        val view = inflater!!.inflate(R.layout.fragment_dashboard, container, false)
+
+        view.dashboard_worldwide_confirmed.text = confirmed.toString()
+        view.dashboard_worldwide_recovered.text = recovered.toString()
+        view.dashboard_worldwide_death.text = death.toString()
+
+        return view
     }
 }
